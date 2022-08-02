18d3f69240ca6fd1cef1ade1694ceb94ac5105a7
aymanBG <40396098+aymanBG@users.noreply.github.com>
2022-08-02 15:42:00 +0200

    Add files via upload

---
+++ js/plugins/summernote/summernote-fr-FR.js
@@ -0,0 +1,93 @@
+(function ($) {
+  $.extend($.summernote.lang, {
+    'fr-FR': {
+      font: {
+        bold: 'Gras',
+        italic: 'italique',
+        underline: 'souligner',
+        strikethrough: 'barré',
+        clear: '清除格式',
+        height: '行高',
+        name: 'police',
+        size: 'Police Taille'
+      },
+      image: {
+      
+      },
+      link: {
+        link: '链接',
+        insert: '插入链接',
+        unlink: '去除链接',
+        edit: '编辑链接',
+        textToDisplay: '显示文本',
+        url: '链接地址',
+        openInNewWindow: '在新窗口打开'
+      },
+      video: {
+        video: '视频',
+        videoLink: '视频链接',
+        insert: '插入视频',
+        url: '视频地址',
+        providers: '(优酷, Instagram, DailyMotion, Youtube等)'
+      },
+      table: {
+        table: '表格'
+      },
+      hr: {
+        insert: '水平线'
+      },
+      style: {
+        style: 'style',
+        normal: 'normal',
+        blockquote: '引用',
+        pre: 'code source',
+        h1: 'Titre 1',
+        h2: 'Titre 2',
+        h3: 'Titre 3',
+        h4: 'Titre 4',
+        h5: 'Titre 5',
+        h6: 'Titre 6'
+      },
+      lists: {
+        unordered: '无序列表',
+        ordered: '有序列表'
+      },
+      options: {
+        help: '帮助',
+        fullscreen: '全屏',
+        codeview: '源代码'
+      },
+      paragraph: {
+        paragraph: '段落',
+        outdent: '减少缩进',
+        indent: '增加缩进',
+        left: '左对齐',
+        center: '居中对齐',
+        right: '右对齐',
+        justify: '两端对齐'
+      },
+      color: {
+        recent: '最近使用',
+        more: '更多',
+        background: '背景',
+        foreground: '前景',
+        transparent: '透明',
+        setTransparent: '透明',
+        reset: '重置',
+        resetToDefault: '默认'
+      },
+      shortcut: {
+        shortcuts: '快捷键',
+        close: '关闭',
+        textFormatting: '文本格式',
+        action: '动作',
+        paragraphFormatting: '段落格式',
+        documentStyle: '文档样式'
+      },
+      history: {
+        undo: '撤销',
+        redo: '重做'
+      }
+    }
+  });
+})(jQuery);
